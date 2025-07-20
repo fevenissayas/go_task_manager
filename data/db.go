39f034aa3b395a0a65db107cd41c795fd18edece
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-07-20 10:25:14 +0300

    add mongodb connection helper

---
+++ data/db.go
@@ -0,0 +1,33 @@
+package data
+
+import (
+	"context"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var TaskCollection *mongo.Collection
+
+func InitDB() {
+	clientOptions := options.Client().ApplyURI(("mongodb://localhost:27017"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		log.Fatal("Mongo connection error: ", err)
+	}
+
+	err = client.Ping(ctx, nil)
+
+	if err != nil {
+		log.Fatal("Mongo ping error:", err)
+	}
+
+	TaskCollection = client.Database("taskdb").Collection("tasks")
+	log.Println("Connected to MongoDB")
+}
