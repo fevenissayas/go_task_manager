98ee198d7f0d46a0786c6ab67cd98648dca25441
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-07-22 11:01:34 +0300

    added user collection initialization

--- data/db.go
+++ data/db.go
@@ -9,6 +9,7 @@ import (
 )
 
 var TaskCollection *mongo.Collection
+var UserCollection *mongo.Collection
 
 func InitDB() {
 	clientOptions := options.Client().ApplyURI(("mongodb://localhost:27017"))
@@ -29,5 +30,6 @@ func InitDB() {
 	}
 
 	TaskCollection = client.Database("taskdb").Collection("tasks")
+	UserCollection = client.Database("userdb").Collection("users")
 	log.Println("Connected to MongoDB")
 }
