f8005fd0e587e57bd540d178f278aef3e25af9ec
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-07-22 11:26:36 +0300

    added user signup, login, promotion, get, and delete controllers with JWT

---
+++ controllers/user_controller.go
@@ -0,0 +1,118 @@
+package controllers
+
+import (
+	"net/http"
+	"restfulapi/data"
+	"restfulapi/models"
+	"time"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var jwtSecret = []byte("my_secret_key_but_will_be_replaced_by_env_later") 
+
+func GenerateJWT(userID primitive.ObjectID) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID.Hex(),
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func Signup(ctx *gin.Context) {
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := data.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "user created", "id": id.Hex()})
+}
+
+func Login(ctx *gin.Context) {
+	var input struct {
+		Username string `json: "username"`
+		Password string `json: "password"`
+	}
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	user, err := data.AuthenticateUser(input.Username, input.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid email or password"})
+		return
+	}
+	token, err := GenerateJWT(user.ID)
+		if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate token"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"token": token})
+
+}
+
+func GetUsers(ctx *gin.Context) {
+	users, err := data.GetAllUsers()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch users"})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+func PromoteUser(ctx *gin.Context) {
+	var input struct {
+		UserID  string `json:"user_id"`
+		NewRole string `json:"new_role"`
+	}
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	userID, err := primitive.ObjectIDFromHex(input.UserID)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	if input.NewRole == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "New role must be provided"})
+		return
+	}
+
+	err = data.PromoteUser(userID, input.NewRole)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "User role updated", "new_role": input.NewRole})
+}
+
+func DeleteUser(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "User ID is required"})
+		return
+	}
+	err := data.DeleteUser(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
+}
