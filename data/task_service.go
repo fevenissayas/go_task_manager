810de01e5d4b3a3e3dba45e51959358a7f2155d2
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-07-22 10:59:18 +0300

    ensure delete returns nil on successful deletion

--- data/task_service.go
+++ data/task_service.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"errors"
 	"restfulapi/models"
 	"time"
 	"go.mongodb.org/mongo-driver/bson"
@@ -67,8 +68,16 @@ func Delete(id string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err = TaskCollection.DeleteOne(ctx, bson.M{"_id": objID})
-	return err
+	result, err := TaskCollection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		return err
+	}
+	
+	if result.DeletedCount == 0 {
+		return errors.New("task not found")
+	}
+
+	return nil
 }
 
 func Update(id string, updatedTask *models.Task) error {
