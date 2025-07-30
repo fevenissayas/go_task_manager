014f7da3f148d7d4caa03391209957ee0b747afd
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-07-30 09:34:49 +0300

    update main.go entry

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"restfulapi/data"
-	"restfulapi/router"
+    "log"
+    "restfulapi/Delivery/controllers"
+    "restfulapi/Delivery/routers"
+    domain "restfulapi/Domain"
+    infrastructure "restfulapi/Infrastructure"
+    repositories "restfulapi/Repositories"
+    usecases "restfulapi/Usecases"
 )
 
-
-
 func main() {
-	data.InitDB()
-	r := router.SetupRouter()
-	r.Run()
+    client, err := infrastructure.ConnectMongo("mongodb://localhost:27017")
+    if err != nil {
+        log.Fatal("Failed to connect to MongoDB:", err)
+    }
+
+    taskCollection := client.Database("taskdb").Collection("tasks")
+    userCollection := client.Database("userdb").Collection("users")
+
+    var taskRepo domain.TaskRepository = repositories.NewMongoTaskRepository(taskCollection)
+    var userRepo domain.UserRepository = repositories.NewMongoUserRepository(userCollection)
+
+    taskUsecase := usecases.NewTaskUsecase(taskRepo)
+    userUsecase := usecases.NewUserUsecase(userRepo)
+
+
+	taskController := controllers.NewTaskController(taskUsecase)
+	userController := controllers.NewUserController(userUsecase)
+
+	router := routers.SetupRouter(taskController, userController, userUsecase)
+
+	router.Run(":8080")
 }
